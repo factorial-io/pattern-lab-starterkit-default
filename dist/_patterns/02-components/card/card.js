46d4e880db8dcebce635966b7d7c65a8bcec1ec0
Milan Matull <milan@factorial.io>
2019-09-08 11:57:34 +0200

    Fix: Don't read theme.json (#5)

--- dist/_patterns/02-components/card/card.js
+++ dist/_patterns/02-components/card/card.js
@@ -1,4 +1,6 @@
-const { variables } = require("../../00-theme/theme.json");
-
-// eslint-disable-next-line no-console
-console.log("card.js loaded, card-color: ", variables.CardColor);
+export default class Card {
+  constructor() {
+    // eslint-disable-next-line no-console
+    console.log("Card");
+  }
+}
